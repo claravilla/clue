16d09ec829c1765d17ee3bf12c4797e845094fa7
betilla x <aclara_77@yahoo.com>
2021-12-19 12:36:06 +0000

    change variable to obj to display the solution with better name

--- script.js
+++ script.js
@@ -1,20 +1,124 @@
-//variables
+//---GAME VARIABLES----
+const green = {
+  name: "Rev Green",
+  value: "green",
+};
 
-const people = ["green", "mustard", "peacock", "plum", "scarlett", "white"];
+const mustard = {
+  name: "Colonel Mustard",
+  value: "mustard",
+};
+
+const peacock = {
+  name: "Mrs peacock",
+  value: "peacock",
+};
+
+const plum = {
+  name: "Professor Plum",
+  value: "plum",
+};
+
+const scarlett = {
+  name: "Miss Scarlett",
+  value: "scarlett",
+};
+
+const white = {
+  name: "Mrs White",
+  value: "white",
+};
+
+const people = [green, mustard, peacock, plum, scarlett, white];
+
+const billard = {
+  name: "Billiard Room",
+  value: "billiard",
+};
+
+const ballroom = {
+  name: "Ballroom",
+  value: "ballroom",
+};
+
+const conservatory = {
+  name: "Conservatory",
+  value: "conservatory",
+};
+
+const dining = {
+  name: "Dining Room",
+  value: "dining",
+};
+
+const hall = {
+  name: "Hall",
+  value: "hall",
+};
+
+const kitchen = {
+  name: "Kitchen",
+  value: "kitchen",
+};
+
+const library = {
+  name: "Library",
+  value: "library",
+};
+
+const lounge = {
+  name: "Lounge",
+  value: "lounge",
+};
+
+const study = {
+  name: "Study",
+  value: "study",
+};
 
 const rooms = [
-  "billard",
-  "ballroom",
-  "conservatory",
-  "dining",
-  "hall",
-  "kitchen",
-  "library",
-  "lounge",
-  "study",
+  billard,
+  ballroom,
+  conservatory,
+  dining,
+  hall,
+  kitchen,
+  library,
+  lounge,
+  study,
 ];
 
-const weapons = ["candlestick", "dagger", "pipe", "revolver", "rope", "wrench"];
+const candlestick = {
+  name: "Candlestick",
+  value: "candlestick",
+};
+
+const dagger = {
+  name: "Dagger",
+  value: "dagger",
+};
+
+const pipe = {
+  name: "Lead Pipe",
+  value: "pipe",
+};
+
+const revolver = {
+  name: "Revolver",
+  value: "revolver",
+};
+
+const rope = {
+  name: "Rope",
+  value: "rope",
+};
+
+const wrench = {
+  name: "Wrench",
+  value: "wrench",
+};
+
+const weapons = [candlestick, dagger, pipe, revolver, rope, wrench];
 
 let game = {
   person: "",
@@ -23,14 +127,15 @@ let game = {
 };
 
 let rounds = 0;
+
+//---FUNCTIONS TO RUN THE GAME---
+
 //eventListeners
 document.getElementById("start-game").addEventListener("click", startGame);
 document.getElementById("submit-guess").addEventListener("click", checkGuess);
 document.getElementById("try-again").addEventListener("click", tryAgain);
 document.getElementById("play-again").addEventListener("click", playAgain);
-document.getElementById("play-again").addEventListener("click", giveSolution);
-
-//functions to run the game
+document.getElementById("solution").addEventListener("click", giveSolution);
 
 //select a random element in an array
 function randomSelection(arr) {
@@ -48,7 +153,9 @@ function startGame() {
 
   document.getElementById("start").setAttribute("class", "invisible");
   document.getElementById("guess-form").removeAttribute("class");
-  document.getElementById("guess-form").setAttribute("class", "guess-form-visible");
+  document
+    .getElementById("guess-form")
+    .setAttribute("class", "guess-form-visible");
 }
 
 //to be called on form submit - check that value submitted matched the game solution and display the right/wrong section
@@ -61,88 +168,72 @@ function checkGuess() {
   console.log(guessPerson, guessRoom, guessWeapon);
   let rightGuess = 0;
 
-
-
-  if (guessPerson === game.person) {
+  if (guessPerson === game.person.value) {
     rightGuess++;
-  };
+  }
 
-  if (guessRoom === game.room ){
+  if (guessRoom === game.room.value) {
     rightGuess++;
- };
+  }
 
- if (guessWeapon === game.weapon ){
-  rightGuess++;
-};
+  if (guessWeapon === game.weapon.value) {
+    rightGuess++;
+  }
 
-
-if (rightGuess===3) {
-  document.getElementById("guess-form").removeAttribute("class"); // set form to not display
-  document.getElementById("guess-form").setAttribute("class", "invisible");
-  document.getElementById("final-guess-count").textContent = `Well done Detective, you solved the murder at your ${rounds} accusation!`
-  document.getElementById("right-answer").classList.remove("invisible"); //set right paragraph to show
-
-} else {
-     document.getElementById("guess-form").removeAttribute("class"); // set form to not display
+  if (rightGuess === 3) {
+    console.log(rounds);
+    document.getElementById("guess-form").removeAttribute("class"); // set form to not display
     document.getElementById("guess-form").setAttribute("class", "invisible");
-    document.getElementById("right-guess").innerHTML = `Accusation #${rounds} <br> Right guesses: ${rightGuess}`;
+    document.getElementById(
+      "solution-par"
+    ).textContent = `Well done Detective, you solved the murder at your ${rounds} accusation!`;
+    document.getElementById("right-answer").classList.remove("invisible"); //set right paragraph to show
+  } else {
+    document.getElementById("guess-form").removeAttribute("class"); // set form to not display
+    document.getElementById("guess-form").setAttribute("class", "invisible");
+    document.getElementById(
+      "right-guess"
+    ).innerHTML = `Accusation #${rounds} <br> Right guesses: ${rightGuess}`;
     document.getElementById("wrong-answer").classList.remove("invisible"); // set wrong answer to display
-
-}
-
-//------- to check guess without telling the user how many they got right
-  // if (
-  //   guessPerson === game.person &&
-  //   guessRoom === game.room &&
-  //   guessWeapon === game.weapon
-  // ) {
-  //   document.getElementById("guess-form").removeAttribute("class"); // set form to not display
-  //   document.getElementById("guess-form").setAttribute("class", "invisible");
-  //   document.getElementById("right-answer").classList.remove("invisible"); //set right paragraph to show
-  // } else {
-  //   document.getElementById("guess-form").removeAttribute("class"); // set form to not display
-  //   document.getElementById("guess-form").setAttribute("class", "invisible");
-  //   document.getElementById("wrong-answer").classList.remove("invisible"); // set wrong answer to display
-  // }
-//------------------------------------------------------------------------------------------------
-
-  
+  }
 }
 
 //to be called on try again - display the form again
 function tryAgain() {
   // set form to display
-  document.getElementById("person").value ="" ;
-  document.getElementById("room").value ="" ;
-  document.getElementById("weapon").value ="" ;
- 
+  document.getElementById("person").value = "";
+  document.getElementById("room").value = "";
+  document.getElementById("weapon").value = "";
+
   document.getElementById("guess-form").classList.remove("invisible");
-  
+
   // set wrong answer not to  display
   document.getElementById("wrong-answer").classList.add("invisible");
 
   //empty the right guess sentence
-  document.getElementById("right-guess").innerHTML="";
+  document.getElementById("right-guess").innerHTML = "";
 }
 
-
-function giveSolution(){
-    //
+//to be called on h. poirot - reveal solutions
+function giveSolution() {
+  document.getElementById("wrong-answer").setAttribute("class", "invisible"); // set wrong answer to not display
+  document.getElementById(
+    "solution-par"
+  ).innerHTML = `After wrong accusation #${rounds} you decided to call Hercule Poirot to help you. <br>
+     Poirot says: <br>"${game.person.name} killed Dr. Black in the ${game.room.name} with the ${game.weapon.name}."`;
+  document.getElementById("right-answer").classList.remove("invisible");
 }
-
 
 //to be called on play again - remove the right paragraph and re-start the game
 function playAgain() {
   // set right answer not to  display
-  document.getElementById("person").value ="" ;
-  document.getElementById("room").value ="" ;
-  document.getElementById("weapon").value ="" ;
-  document.getElementById("final-guess-count").textContent = "";
+  document.getElementById("person").value = "";
+  document.getElementById("room").value = "";
+  document.getElementById("weapon").value = "";
+  document.getElementById("solution-par").textContent = "";
   document.getElementById("right-answer").classList.add("invisible");
 
   startGame();
 }
-
-
 
 //add round counter
